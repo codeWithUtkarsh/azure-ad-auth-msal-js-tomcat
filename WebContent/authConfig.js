082e83950f0d476400077c539ceb7c455d5a64ed
Utkarsh Kumar Sharma <utkarshkviim@gmail.com>
2022-03-22 01:39:02 +0530

    initial commit

---
+++ WebContent/authConfig.js
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+ 
+const msalConfig = {
+    auth: {
+        clientId: "4c64d674-eb64-480a-a8fe-17a99b0643ff",
+        authority: "https://login.microsoftonline.com/07e373ad-d334-46f4-a926-dd2f68a616c0/",
+        redirectUri: "http://localhost:8080/app/",
+    },
+    cache: {
+        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {	
+        loggerOptions: {	
+            loggerCallback: (level, message, containsPii) => {	
+                if (containsPii) {		
+                    return;		
+                }		
+                switch (level) {		
+                    case msal.LogLevel.Error:		
+                        console.error(message);		
+                        return;		
+                    case msal.LogLevel.Info:		
+                        console.info(message);		
+                        return;		
+                    case msal.LogLevel.Verbose:		
+                        console.debug(message);		
+                        return;		
+                    case msal.LogLevel.Warning:		
+                        console.warn(message);		
+                        return;		
+                }	
+            }	
+        }	
+    }
+};
+
+const loginRequest = {
+    scopes: ["User.Read"]
+};
+
+const tokenRequest = {
+    scopes: ["User.Read", "Mail.Read"],
+    forceRefresh: false // Set this to "true" to skip a cached token and go to the server to get a new token
+};
